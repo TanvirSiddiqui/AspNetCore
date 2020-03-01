996d6a344995f018afedb28195be194275872077
TanvirSiddiqui <babatanvir@hotmail.com>
2020-03-01 13:38:57 +0600

    Assignmet 02 uploading to its branch

---
+++ MVC_Assignment02.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Assignment02.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Assignment 2 was mistakenly reposited on master branch.
+# Assignment 2 
